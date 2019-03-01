305fdf487b67c69540b1fbafb73dfb235b685e5a
mbasov <lechosquad@gmail.com>
2019-03-01 14:34:59 +0300

    update ui +mobile

---
+++ client/js/main.js
@@ -0,0 +1,17 @@
+function fullScreen() {
+    // Kind of painful, but this is how it works for now
+    if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+        document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+        document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+        document.documentElement.msRequestFullscreen();
+    }
+}
+
+function lock(orientation) {
+    fullScreen();
+    screen.orientation.lock("landscape-primary");
+}
